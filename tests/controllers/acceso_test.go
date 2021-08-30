dab8be6574eb5b4c38c7284d4468830f78cca7e1
Felipe <fvelandia695@gmail.com>
2021-08-30 08:57:35 -0500

    feat: añadidos los archivos de test

--- tests/controllers/acceso_test.go
+++ tests/controllers/acceso_test.go
@@ -20,12 +20,29 @@ func init() {
 }
 
 // TestGet is a sample to run an endpoint test
-func TestGet(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/v1/object", nil)
+func TestGetAutorizacion(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://localhost:8080/v1/acceso/9851/9851/?tipo=out&sede=1096", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
+	beego.Trace("testing", "TestGetAutorizacion", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+}
+
+func TestGetIngreso(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://localhost:8080/v1/acceso/9851/1096/out", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestGetAutorizacion", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 		Convey("Status Code Should Be 200", func() {
